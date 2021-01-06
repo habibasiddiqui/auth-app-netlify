1793f52fa3f695768c77c43516551a82645ceeba
habibasiddiqui <habiba_siddiqui@ymail.com>
2021-01-06 20:35:38 +0500

    homepage

--- src/home.js
+++ src/home.js
@@ -5,12 +5,10 @@ const app = express();
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({
-    hello: "hi!"
-  });
+  res.send('<h1>Homepage</h1>')
 });
 
-app.use(`/.netlify/functions/api`, router);
+app.use(`/.netlify/functions/home`, router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
